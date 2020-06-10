4dbfd0232e318ca84e0aaf084de3dd6615569600
AndroBuddy <anirudhchimpidi8@gmail.com>
2020-06-10 12:30:14 +0000

    FluidCustomization: Init
    - The whole app to this commit was done by @AndroBuddy

---
+++ src/com/fluid/customisation/ui/statusbar/StatusBarFragment.java
@@ -0,0 +1,19 @@
+package com.fluid.customisation.ui.statusbar;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.fluid.customisation.R;
+
+public class StatusBarFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_status_bar, null);
+    }
+}
+
